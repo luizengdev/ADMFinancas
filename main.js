5830dd9b7ad849504361178605174fbdad0aa597
luizeqs <luizengdev@gmail.com>
2022-07-28 14:45:06 -0300

    Estilizando elementos com CSS e criação da função de formatação de valores!

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const janeiro = new Mes("janeiro");
-janeiro.adicionarLancamento(new Lancamento("Salário", "receita", 3000));
+janeiro.adicionarLancamento(new Lancamento("Salário", "receita", 5000));
 janeiro.adicionarLancamento(new Lancamento("Aluguel", "despesa", 1000));
 janeiro.adicionarLancamento(new Lancamento("Conta de Luz", "despesa", 200));
 janeiro.adicionarLancamento(new Lancamento("Conta de Água", "despesa", 100));
@@ -11,7 +11,7 @@ janeiro.adicionarLancamento(new Lancamento("Condominio", "despesa", 300));
 janeiro.adicionarLancamento(new Lancamento("Farmácia", "despesa", 100));
 
 const fevereiro = new Mes("fevereiro");
-fevereiro.adicionarLancamento(new Lancamento("Salário", "receita", 3000));
+fevereiro.adicionarLancamento(new Lancamento("Salário", "receita", 5000));
 fevereiro.adicionarLancamento(new Lancamento("Aluguel", "despesa", 1200));
 fevereiro.adicionarLancamento(new Lancamento("Conta de Luz", "despesa", 250));
 fevereiro.adicionarLancamento(new Lancamento("Conta de Água", "despesa", 100));
@@ -69,14 +69,58 @@ function redenrizar () {
         app.firstChild.remove();
     }
     const painel = document.createElement("div");
+    const cores = ["red", "yellow", "green", "blue"];
+    const grafico = document.createElement("div");
+    grafico.className = "grafico";
+
     for (const mes of ano.meses) {
-        addElement(painel, "h3", mes.nome);
+        const coluna = document.createElement("div");
+        coluna.className = "grafico-coluna";
+        const cor = document.createElement("div");
+        cor.style.height = (mes.totalizador.saldo*100)/10000;
+        cor.style.backgroundColor = cores.pop();
+        coluna.appendChild(cor);
+        const nomeDoMes = document.createElement("div");
+        nomeDoMes.className = "grafico-coluna-texto";
+        nomeDoMes.innerText = mes.nome;
+        coluna.appendChild(cor);
+        coluna.appendChild(nomeDoMes);
+        grafico.appendChild(coluna);
+    }
+    painel.appendChild(grafico);
+
+    for (const mes of ano.meses) {
+        addElement(painel, "h4", mes.nome);
+        const tabelaLancamentos = document.createElement("table");
+        tabelaLancamentos.className = "tabela-lancamentos";
+        
+        const linhaTitulo = document.createElement("tr");
+        addElement(linhaTitulo, "th", "Categoria");
+        addElement(linhaTitulo, "th", "Valor");
+        tabelaLancamentos.appendChild(linhaTitulo);
+
         for (const lancamento of mes.lancamentos) {
-            const detalhesLancamento = lancamento.tipo + " " + lancamento.categoria + " " + lancamento.valor;
-            addElement(painel, "p", detalhesLancamento);
+            const linhaLancamento = document.createElement("tr");
+            addElement(linhaLancamento, "td", lancamento.categoria);
+            addElement(linhaLancamento, "td", formatarDinheiro(lancamento.valor));
+            tabelaLancamentos.appendChild(linhaLancamento);
         }
-        addElement(painel, "h4", mes.totalizador.saldo);
-        addElement(painel, "hr");
+        const linhaJuros = document.createElement("tr");
+        addElement(linhaJuros, "th", "Juros");
+        addElement(linhaJuros, "th", formatarDinheiro(mes.totalizador.juros));
+        tabelaLancamentos.appendChild(linhaJuros);
+
+        const linhaRendimentos = document.createElement("tr");
+        addElement(linhaRendimentos, "th", "Rendimentos");
+        addElement(linhaRendimentos, "th", formatarDinheiro(mes.totalizador.rendimentos));
+        tabelaLancamentos.appendChild(linhaRendimentos);
+
+        const linhaSaldo = document.createElement("tr");
+        addElement(linhaSaldo, "th", "Total");
+        addElement(linhaSaldo, "th", formatarDinheiro(mes.totalizador.saldo));
+        tabelaLancamentos.appendChild(linhaSaldo);
+        painel.appendChild(tabelaLancamentos);
+
     }
     app.appendChild(painel);
 }
@@ -91,11 +135,18 @@ function adicionarLancamento () {
     ano.adicionarLancamento(mes.value, new Lancamento(categoria.value, tipo.value, parseFloat(valor.value)));
     ano.calcularSaldo();
     redenrizar();
-    mes.value= "";
+    mes.value = ano.meses[0].nome;
+    tipo.value = "receita";
     categoria.value = "";
-    tipo.value = "";
     valor.value = "";
 }
 
 const botao = document.getElementById("botao");
 botao.addEventListener("click", adicionarLancamento);
+
+const mesSelect = document.getElementById("mes");
+for (const mes of ano.meses) {
+    const option = document.createElement("option");
+    option.text = mes.nome;
+    mesSelect.add(option);
+}
